378ec49618f0f85f3bcd1fdee203b993ef4d2609
ericagong <48196721+ericagong@users.noreply.github.com>
2021-10-15 20:29:54 +0900

    Submit

--- toh/hero/models.py
+++ toh/hero/models.py
@@ -3,9 +3,14 @@ from django.db.models.fields import related
 
 class Hero(models.Model):
     name = models.CharField(max_length=120)
+    age = models.IntegerField(default=25)
+    score = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
+    
+    def __introduce__(self):
+        return f'Hello, my name is {self.name} and my score is {self.score}!' 
 
 class Team(models.Model):
     name = models.CharField(max_length=120)
